c82dde4e0d28839ea88d421e97f921b15613c368
xorrr <derxor@gmail.com>
2015-06-14 21:19:38 +0200

    provide example scripts to update and rollback

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -1,1 +1,5 @@
-liquibase/liquibase --driver=com.mysql.jdbc.Driver --classpath=mysql-connector-java-5.1.35/mysql-connector-java-5.1.35-bin.jar --changeLogFile=changelog.xml --url="jdbc:mysql://localhost/liquibase" --username=liquibase --password=liquibase migrate
+#/bin/sh
+
+DB_NAME=$1
+
+liquibase/liquibase --driver=com.mysql.jdbc.Driver --classpath=mysql-connector-java-5.1.35/mysql-connector-java-5.1.35-bin.jar --changeLogFile=changelog.xml --url="jdbc:mysql://localhost/$DB_NAME" --username=liquibase --password=liquibase migrate
---
+++ bin/update.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+DB_NAME=$1
+
+liquibase/liquibase --driver=com.mysql.jdbc.Driver --classpath=mysql-connector-java-5.1.35/mysql-connector-java-5.1.35-bin.jar --changeLogFile=changelog.xml --url="jdbc:mysql://localhost/$DB_NAME" --username=liquibase --password=liquibase update
